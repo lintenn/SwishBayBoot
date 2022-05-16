00424c3f5621e26c8f92a93b8cbe8d25f0746718
Galo <galopg2001@hotmail.com>
2022-05-16 23:47:53 +0200

    .

--- SwishBayBoot/src/main/java/es/taw/swishbay/controller/PruebaController.java
+++ SwishBayBoot/src/main/java/es/taw/swishbay/controller/PruebaController.java
@@ -2,23 +2,30 @@ package es.taw.swishbay.controller;
 
 import es.taw.swishbay.dao.UsuarioRepository;
 import es.taw.swishbay.entity.Usuario;
+import es.taw.swishbay.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 public class PruebaController {
 
+    private UsuarioService usuarioService;
+
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    public void setUsuarioService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
     @GetMapping("/Usuarios")
     public String inicio (Model model, HttpSession sesion) {
 
-        List<Usuario> lista = this.usuarioRepository.findAll();
+        List<Usuario> lista = this.usuarioService.findAll();
         model.addAttribute("usuarios", lista);
 
 
