c9017f37482b4ecbbbc0b1a6eca757016235d4f3
Galo <galopg2001@hotmail.com>
2022-05-16 10:12:49 +0200

    Inicial

---
+++ SwishBayBoot/src/main/java/es/taw/swishbay/SwishBayApplication.java
@@ -0,0 +1,13 @@
+package es.taw.swishbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwishBayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwishBayApplication.class, args);
+    }
+
+}
