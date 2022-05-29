f8c2dd3122273010f9b12607309d9aaa8338d98c
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-05-29 22:04:48 +0200

    Add queries in ProductoRepository

--- SwishBayBoot/src/main/java/es/taw/swishbay/dao/ProductoRepository.java
+++ SwishBayBoot/src/main/java/es/taw/swishbay/dao/ProductoRepository.java
@@ -84,4 +84,18 @@ public interface ProductoRepository  extends JpaRepository<Producto, Integer> {
 
     @Query("select pu from Puja pu where pu.producto1.id= :pId and pu.pujaPK<>:pujaId")
     List<Puja> findLosers(Integer pId, PujaPK pujaId);
+
+    @Query("select p from Producto p where p.titulo like CONCAT('%', :titulo,'%')  and p.precioSalida <= :precio and p.categoria.nombre like CONCAT('%',:filtroCategoria ,'%')  and NOT (p.vendedor.id = :id) and p.comprador is null")
+    List<Producto> findExistentesByFiltro(String titulo, String filtroCategoria, Double precio, Integer id); //Miguel Oña Guerrero
+
+    @Query("select p from Producto p where p.enPuja = 1 and p.titulo like :titulo and p.precioSalida <= :precio and p.categoria.nombre like :filtroCategoria and NOT (p.vendedor.id = :id) and p.comprador is null")
+    List<Producto> findEnPujaByFiltro(String titulo, String filtroCategoria, Double precio, Integer id); //Miguel Oña Guerrero
+
+    @Query("select p from Producto p join p.usuarioList u where u.id = :id and p.precioSalida <= :precio and p.titulo like CONCAT('%', :titulo,'%') and p.categoria.nombre like CONCAT('%',:filtroCategoria ,'%') and NOT (p.vendedor.id = :id)")
+    List<Producto> findFavoritosByFiltro(String titulo, String filtroCategoria, Double precio, Integer id); //Miguel Oña Guerrero
+
+    @Query("select p from Producto p where p.titulo like CONCAT('%', :titulo,'%') and p.precioSalida <= :precio and p.categoria.nombre like CONCAT('%',:filtroCategoria ,'%') and p.comprador.id = :id")
+    List<Producto> findCompradosByFiltro(String titulo, String filtroCategoria, Double precio, Integer id); //Miguel Oña Guerrero
+
+
 }
