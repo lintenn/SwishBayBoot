f1fad44fed909566d839b80c1a582d80191fac4b
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-05-29 22:04:00 +0200

    Add CompradorService

---
+++ SwishBayBoot/src/main/java/es/taw/swishbay/service/CompradorService.java
@@ -0,0 +1,61 @@
+package es.taw.swishbay.service;
+
+import es.taw.swishbay.dao.ProductoRepository;
+import es.taw.swishbay.dto.ProductoDTO;
+import es.taw.swishbay.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Service para mostrar productos al comprador
+ *
+ * @author Miguel Oña Guerrero
+ */
+
+@Service
+public class CompradorService {
+
+    private ProductoRepository productoRepository;
+
+    @Autowired
+    public void setProductoService(ProductoRepository productoRepository){
+        this.productoRepository = productoRepository;
+    }
+
+    private List<ProductoDTO> listaProductoEntityADTO (List<Producto> lista) {
+        List<ProductoDTO> listaDTO = null;
+        if (lista != null) {
+            listaDTO = new ArrayList<>();
+            for (Producto p : lista) {
+                listaDTO.add(p.toDTO());
+            }
+        }
+        return listaDTO;
+    }
+
+    public List<ProductoDTO> listarProductosExistentes(String filtroTitulo, String filtroCategoria, Double filtroPrecio, int idUsuario){ // Miguel
+        List<Producto> productos = productoRepository.findExistentesByFiltro(filtroTitulo, filtroCategoria, filtroPrecio, idUsuario);
+
+        return listaProductoEntityADTO(productos);
+    }
+
+    public List<ProductoDTO> listarProductosFavoritos(String filtroTitulo, String filtroCategoria, Double filtroPrecio, int idUsuario){ // Miguel
+        List<Producto> productos = productoRepository.findFavoritosByFiltro(filtroTitulo, filtroCategoria, filtroPrecio, idUsuario);
+
+        return this.listaProductoEntityADTO(productos);
+    }
+
+    public List<ProductoDTO> listarProductosComprados(String filtroTitulo, String filtroCategoria, Double filtroPrecio, int idUsuario){ // Miguel
+        List<Producto> productos = productoRepository.findCompradosByFiltro(filtroTitulo, filtroCategoria, filtroPrecio, idUsuario);
+
+        return this.listaProductoEntityADTO(productos);
+    }
+
+    public List<ProductoDTO> listarProductosEnPuja(String filtroTitulo, String filtroCategoria, Double filtroPrecio, int idUsuario){ // Miguel
+        List<Producto> productos = productoRepository.findEnPujaByFiltro(filtroTitulo, filtroCategoria, filtroPrecio, idUsuario);
+
+        return this.listaProductoEntityADTO(productos);
+    }
+}
